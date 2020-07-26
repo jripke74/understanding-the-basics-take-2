0e7a6d19f7d60b7534a568dc15353f2d6da7da64
jripke74 <jripke74@gmail.com>
2020-07-26 08:16:13 -0700

    add req.headers log

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.headers);
+  // process.exit();
 });
 
 server.listen(5000);
