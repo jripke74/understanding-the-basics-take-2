28e699d3e61ea1a4dd16ff0e84faa8a25e86ab38
jripke74 <jripke74@gmail.com>
2020-07-26 09:00:32 -0700

    add route, form

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  // process.exit();
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter Messafe</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button><body>');
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
