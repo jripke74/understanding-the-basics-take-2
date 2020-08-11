6140ecd7be5785630826cd85699c661b326b405f
jripke74 <jripke74@gmail.com>
2020-08-11 12:10:46 -0700

    add user info

--- models/user.js
+++ models/user.js
@@ -1,31 +1,43 @@
 const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
-const ObjectId = mongodb.ObjectID
+const ObjectId = mongodb.ObjectID;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('users')
-      .insertOne(this);
+    return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ productId: new Object(product._id), quantity: 1 }] };
+    const db = getDb();
+    db.collection('users').updateOne(
+      { _id: new Object(this._id) },
+      { $set: {cart: updatedCart} }
+    );
   }
 
   static findById(userId) {
     const db = getDb();
     return db
       .collection('users')
-      .findOne({ _id: new ObjectId(userId)} )
-      .then(user => {
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
         console.log(user);
         return user;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
